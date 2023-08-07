81af10f596c484f32d425a856891293d41aa4c04
Asilsay <lamufaku@gmail.com>
2023-08-08 00:22:28 +0700

    feat: add state to fetch data

--- src/presentation/pages/Homepage.tsx
+++ src/presentation/pages/Homepage.tsx
@@ -1,23 +1,30 @@
-import { useEffect, useState } from 'react';
+import { Suspense, lazy, useEffect, useState } from 'react';
 
 import { Typography, Row, Col } from 'antd';
 
 import LayoutComp from '../components/LayoutComp';
-import Cards from '../components/Cards';
+// import Cards from '../components/Cards';
 import withReactContent from 'sweetalert2-react-content';
 import swal from '../../data/utils/swal';
 import { NewsItem } from '../../data/types/newsType';
 import api from '../../domain/api/api';
+import useCategoryStore from '../../domain/store/categoryNews';
+import LoadingAll from '../components/LoadingAll';
+
+const Cards = lazy(() => import('../components/Cards'));
 
 const { Title } = Typography;
 
 function Homepage() {
   const [newsData, setNewsData] = useState<NewsItem[]>([]);
   const MySwal = withReactContent(swal);
+  const selectedCategory = useCategoryStore((state) => state.selectedCategory);
+  const [load, setLoad] = useState<boolean>(false);
 
   const fetchDatas = async () => {
+    setLoad(true);
     await api
-      .GetTop('general')
+      .GetTop(selectedCategory)
       .then((response) => {
         const { articles } = response.data;
         setNewsData(articles);
@@ -29,38 +36,55 @@ function Homepage() {
           text: `error :  ${error.message}`,
           showCancelButton: false,
         });
-      });
+      })
+      .finally(() => setLoad(false));
   };
 
   useEffect(() => {
     fetchDatas();
-  }, []);
+  }, [selectedCategory]);
 
   return (
     <LayoutComp>
-      <Title style={{ textAlign: 'center' }}>NEWS</Title>
+      {load ? (
+        <LoadingAll />
+      ) : (
+        <>
+          <Title
+            style={{
+              textAlign: 'center',
+              textTransform: 'uppercase',
+              margin: '30px 0',
+            }}
+          >
+            NEWS {selectedCategory !== 'general' ? selectedCategory : ''}
+          </Title>
 
-      <div>
-        <Row gutter={[16, 16]}>
-          {newsData.map((news, index) => (
-            <Col
-              key={index}
-              xs={24}
-              sm={12}
-              md={8}
-              lg={6}
-              style={{ display: 'flex', marginBottom: '12px' }}
-            >
-              <Cards
-                title={news.title}
-                description={news.description}
-                imageUrl={news.urlToImage}
-                url={news.url}
-              />
-            </Col>
-          ))}
-        </Row>
-      </div>
+          <div>
+            <Suspense fallback={<LoadingAll />}>
+              <Row gutter={[16, 16]}>
+                {newsData.map((news, index) => (
+                  <Col
+                    key={index}
+                    xs={24}
+                    sm={12}
+                    md={8}
+                    lg={6}
+                    style={{ display: 'flex', marginBottom: '12px' }}
+                  >
+                    <Cards
+                      title={news.title}
+                      description={news.description}
+                      imageUrl={news.urlToImage}
+                      url={news.url}
+                    />
+                  </Col>
+                ))}
+              </Row>
+            </Suspense>
+          </div>
+        </>
+      )}
     </LayoutComp>
   );
 }
